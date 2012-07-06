8782a6591067087f066e538ec4d14555dcebc1f9
Alicja <acyganiewicz@gmail.com>
2012-07-06 03:10:17 +0200

    Stats: form, result templates

---
+++ CMS/public_html/stats.php
@@ -0,0 +1,30 @@
+<?php
+    /**
+     * Short URL stats
+     *
+     * @package stats.php
+     * @author Alicja Cyganiewicz
+     * @since 05.07.2012
+     */
+	 
+	 include 'cms.h.php';
+	 
+	if(isset($_POST['short_url'])){
+		$site['whole_url'] = $_POST['short_url'];
+		$part_url = substr($site['whole_url'], -6, 6);
+		$site['url'] = current(download_data($sql, 'Url', array('*'), array('short_url' => $part_url)));
+		
+		if(count($site['url'])){
+			$site['content'] = 'stats_result';
+		}
+		else{
+			$site['content'] = 'form';
+			$site['errors'] = 'No matching URL in Database. Try again.';
+		}
+	}
+	else{
+		$site['content'] = 'form';
+	}
+	
+	screen_site($paths, $templates, $site);
+?>
